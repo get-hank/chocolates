a46c5d55d2aa1ebe5f782cbee01da8f785cd6eb0
Jack <jackratner@gmail.com>
2020-12-08 09:23:39 -0500

    feat: build out colors and theming
    - add colors
    - add border box sizing to all children of theme provider
    - respect theme typography in typography components

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { colors } from "../util/colors";
 
 export const defaultTheme = {
   typography: {
     baseSize: 1, // rem
-    headingType: "'Poppins', sans serif",
-    baseType: "'Lato', sans serif",
+    headingType: '"Poppins", sans-serif',
+    baseType: '"Lato", sans-serif',
+  },
+  colors: {
+    primary: colors.primary700,
+    primaryDark: colors.primary900,
+    primaryLight: colors.primary100,
+    white: colors.white,
+    black: colors.black,
+    grayLight: colors.gray10,
+    text: colors.gray950,
+    textDisabled: colors.gray200,
+    grayBorder: colors.gray100,
   },
 };
 
@@ -13,11 +25,17 @@ type ThemeProps = {
   theme?: any;
 };
 
+const Wrapper = styled.div`
+  * {
+    box-sizing: border-box;
+  }
+`;
+
 export const DefaultThemeProvider: React.FunctionComponent<ThemeProps> = ({
   theme,
   children,
 }) => (
     <ThemeProvider theme={{ ...defaultTheme, ...theme }}>
-      {children}
+      <Wrapper>{children}</Wrapper>
     </ThemeProvider>
   );
---
+++ src/stories/button.stories.tsx
@@ -0,0 +1,23 @@
+// @ts-nocheck
+
+import React, { ComponentProps } from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { Container } from "../components/grid";
+import Button from "../components/Button";
+
+export default {
+  title: "Button",
+  component: Button,
+} as Meta;
+
+const Template: Story<ComponentProps<typeof Button>> = (args) => (
+  <Container center>
+    <Button {...args} />
+  </Container>
+);
+
+export const Standard = Template.bind({});
+Standard.args = {
+  children: "Click me",
+  disabled: false,
+};
