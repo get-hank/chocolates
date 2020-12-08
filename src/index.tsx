5398a82d97e038ac1c15fb074ac181696233ff90
Jack <jackratner@gmail.com>
2020-12-08 14:51:39 -0500

    fix: export colors from index file

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ export * from "./components/spacing";
 export * from "./components/typography";
 export * from "./util/helpers";
 export * from "./util/layout";
+export * from "./util/colors";
 export { default as Button } from "./components/Button";
 export { default as Tabs } from "./components/Tabs";
 export { default as Table } from "./components/Table";
