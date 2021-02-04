32c914832c3771dda305560a2367377bc2b91c72
Jack <jackratner@gmail.com>
2021-02-04 11:22:12 -0500

    feat: provide reload to query wrapper tree using context

--- src/api.tsx
+++ src/api.tsx
@@ -1,4 +1,5 @@
 export * from "./util/api";
+export * from "./components/QueryWrapper";
 export { default as QueryWrapper } from "./components/QueryWrapper";
 export * from "./components/impersonation";
 export { default as AddressModal } from "./components/AddressModal";
--- src/components/QueryWrapper.tsx
+++ src/components/QueryWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { QueryRenderer, FetchPolicy } from "react-relay";
 import { useApiToken } from "../util/api";
 import {
@@ -19,9 +19,16 @@ type renderType = React.ComponentProps<typeof QueryRenderer>["render"];
 type RenderedQueryContainerProps = Parameters<renderType>[0] & {
   render: renderType;
 };
+
+type QueryContextType = { reload: () => void };
+export const RelayQueryContext = createContext<QueryContextType>({
+  reload: () => { },
+});
+
 const RenderedQueryContainer = ({
   render,
   props,
+  retry,
   ...rest
 }: RenderedQueryContainerProps) => {
   const [priorResponse, setPriorResponse] = useState<unknown | null>(null);
@@ -30,7 +37,15 @@ const RenderedQueryContainer = ({
     if (props && priorResponse !== props) setPriorResponse(props);
   }, [props, priorResponse, setPriorResponse]);
 
-  return <>{render({ ...rest, props: props || priorResponse })}</>;
+  return (
+    <RelayQueryContext.Provider value={{ reload: retry }}>
+      {render({
+        retry,
+        ...rest,
+        props: props || priorResponse,
+      })}
+    </RelayQueryContext.Provider>
+  );
 };
 
 const QueryWrapper: React.FC<QueryWrapperProps> = ({
