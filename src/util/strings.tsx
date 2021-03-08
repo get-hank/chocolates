f81b86205d465faa3f6c0b513d1aff5a52489675
Jack <jackratner@gmail.com>
2021-03-08 13:08:53 -0500

    feat: toSentence string array utility

--- src/util/strings.tsx
+++ src/util/strings.tsx
@@ -1,3 +1,11 @@
 export const capitalize = (str: string) => {
   return str[0].toUpperCase() + str.substring(1);
 };
+
+export const toSentence = (strs: string[]) => {
+  if (strs.length === 0) return "";
+  if (strs.length === 1) return strs[0];
+  return strs.reduce((sentence, str, idx) =>
+    idx === strs.length - 1 ? `${sentence} and ${str}` : `${sentence}, ${str}`
+  );
+};
