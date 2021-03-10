fb740752cf90187a358603eb70a3d894cdcd6ced
Jack <jackratner@gmail.com>
2021-03-10 17:54:48 -0500

    feat: return errors object as fieldErrors from request

--- src/util/api.tsx
+++ src/util/api.tsx
@@ -76,50 +76,64 @@ export const defaultHeaders = (apiToken: string) => {
 
 export const request: (
   r: requestArgs
-) => Promise<{ error?: Error; body?: any }> = async ({
-  apiBase,
-  apiToken,
-  path,
-  method,
-  body,
-}) => {
-    const impersonateId = impersonatingId();
+) => Promise<{
+  error?: Error;
+  fieldErrors?: { [field: string]: string };
+  body?: any;
+}> = async ({ apiBase, apiToken, path, method, body }) => {
+  const impersonateId = impersonatingId();
+  try {
+    const res = await fetch(`${apiBase}/${path}`, {
+      method: method ? method : "get",
+      body: body ? JSON.stringify(body) : null,
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+        "Content-Type": "application/json",
+        ...(impersonateId && { "X-Impersonate-Id": impersonateId }),
+      },
+    });
+
+    let responseBody = null;
     try {
-      const res = await fetch(`${apiBase}/${path}`, {
-        method: method ? method : "get",
-        body: body ? JSON.stringify(body) : null,
-        headers: {
-          Authorization: `Bearer ${apiToken}`,
-          "Content-Type": "application/json",
-          ...(impersonateId && { "X-Impersonate-Id": impersonateId }),
-        },
+      responseBody = await res.json();
+    } catch (error) {
+      if (res.status !== 204) return { error };
+    }
+
+    if (!res.ok) {
+      if (responseBody.errors && responseBody.errors.length) {
+        return { error: new Error(responseBody.errors[0]), body: responseBody };
+      } else if (responseBody.errors) {
+        const errors = responseBody.errors as {
+          [field: string]: string[] | string;
+        };
+        return {
+          body: responseBody,
+          fieldErrors: Object.keys(errors).reduce((errs, key) => {
+            const val = errors[key];
+            if (Array.isArray(val)) {
+              errs[key] = val[0] as string;
+            } else {
+              errs[key] = val as string;
+            }
+            return errs;
+          }, {} as { [field: string]: string }),
+        };
+      } else if (responseBody.error) {
+        return { error: new Error(responseBody.error), body: responseBody };
+      }
+      return { error: new Error("Something went wrong") };
+    }
+
+    // reset expiration on impersonation cookie upon successful API request
+    if (impersonateId)
+      impersonate(impersonateId, {
+        apiBase,
+        storage: impersonateStorageType(),
       });
 
-      let responseBody = null;
-      try {
-        responseBody = await res.json();
-      } catch (error) {
-        if (res.status !== 204) return { error };
-      }
-
-      if (!res.ok) {
-        if (responseBody.errors && responseBody.errors.length) {
-          return { error: new Error(responseBody.errors[0]), body: responseBody };
-        } else if (responseBody.error) {
-          return { error: new Error(responseBody.error), body: responseBody };
-        }
-        return { error: new Error("Something went wrong") };
-      }
-
-      // reset expiration on impersonation cookie upon successful API request
-      if (impersonateId)
-        impersonate(impersonateId, {
-          apiBase,
-          storage: impersonateStorageType(),
-        });
-
-      return { body: responseBody };
-    } catch (err) {
-      return { error: err };
-    }
-  };
+    return { body: responseBody };
+  } catch (err) {
+    return { error: err };
+  }
+};
