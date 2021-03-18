0a9f68a782c0fa5766029f5eaa39f6f042183a77
Jack <jackratner@gmail.com>
2021-03-18 18:21:44 -0400

    feat: allow color override on close component

--- src/icons/Close.tsx
+++ src/icons/Close.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Close = () => (
+const Close = ({ color = "#000" }: { color?: string }) => (
   <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M20 4L4 20M4 4l16 16"
-      stroke="#000"
+      stroke={color}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
