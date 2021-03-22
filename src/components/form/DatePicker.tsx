1961ca3618581649811fafac711e4df9fb767d92
Jack <jackratner@gmail.com>
2021-03-22 11:26:33 -0400

    feat: allow null initial value on datepicker

--- src/components/form/DatePicker.tsx
+++ src/components/form/DatePicker.tsx
@@ -1,17 +1,16 @@
-import React, { useCallback, useState } from "react";
-import styled from "styled-components";
-import { DateTime } from "luxon";
-import CalendarPicker from "../CalendarPicker";
-import { InputStyle, _InputProps } from "./Input";
-import { isMobileViewport } from "../../util/layout";
+import React, { useState } from 'react'
+import { DateTime } from 'luxon'
+import CalendarPicker from '../CalendarPicker'
+import { InputStyle, _InputProps } from './Input'
+import { isMobileViewport } from '../../util/layout'
 
 export type DatePickerProps = _InputProps & {
-  onDateChange: (d: DateTime) => any;
+  onDateChange: (d: DateTime) => any
   pickerProps?: {
-    minDate?: DateTime;
-    maxDate?: DateTime;
-  };
-};
+    minDate?: DateTime
+    maxDate?: DateTime
+  }
+}
 
 export const DatePicker = ({
   styledProps = {},
@@ -19,28 +18,32 @@ export const DatePicker = ({
   nativeProps,
   pickerProps = {},
 }: DatePickerProps) => {
-  const { defaultValue, onChange, ...rest } = nativeProps;
-  const [value, setValue] = useState<DateTime>(
-    defaultValue ? DateTime.fromISO(defaultValue as string) : DateTime.local()
-  );
-  const [pickerVisible, setPickerVisible] = useState(false);
-  const mobileViewport = isMobileViewport();
+  const { defaultValue, onChange, ...rest } = nativeProps
+  const [value, setValue] = useState<DateTime | null>(
+    defaultValue ? DateTime.fromISO(defaultValue as string) : null
+  )
+  const [pickerVisible, setPickerVisible] = useState(false)
+  const mobileViewport = isMobileViewport()
 
   const dateChanged = (newDate: Date[] | Date | string) => {
     const newDateLuxon =
-      typeof newDate === "string"
+      typeof newDate === 'string'
         ? DateTime.fromISO(newDate)
         : // TODO: update this when implementing date range picking
         Array.isArray(newDate)
           ? null
-          : DateTime.fromISO(newDate.toISOString());
-    setValue(newDateLuxon);
-    setPickerVisible(false);
-    onDateChange && onDateChange(newDateLuxon);
-  };
+          : DateTime.fromISO(newDate.toISOString())
+    if (newDateLuxon.invalidExplanation) {
+      console.error(newDateLuxon.invalidExplanation)
+    } else {
+      setValue(newDateLuxon)
+      setPickerVisible(false)
+      onDateChange && onDateChange(newDateLuxon)
+    }
+  }
 
-  const showPicker = !mobileViewport && pickerVisible;
-  const { minDate, maxDate } = pickerProps;
+  const showPicker = !mobileViewport && pickerVisible
+  const { minDate, maxDate } = pickerProps
 
   return (
     <InputStyle {...styledProps}>
@@ -50,16 +53,19 @@ export const DatePicker = ({
         onChange={(e) => dateChanged(e.target.value)}
         value={
           mobileViewport
-            ? value.toFormat("yyyy-MM-dd")
-            : value.toFormat("MM / dd / yyyy")
+            ? value.toFormat('yyyy-MM-dd')
+            : value
+              ? value.toFormat('MM / dd / yyyy')
+              : ''
         }
+        placeholder={!mobileViewport && 'MM / DD / YYYY'}
         {...rest}
-        type={mobileViewport ? "date" : "text"}
+        type={mobileViewport ? 'date' : 'text'}
       />
       {showPicker ? (
         <CalendarPicker
           minDetail="month"
-          defaultValue={new Date(value.valueOf())}
+          defaultValue={value ? new Date(value.valueOf()) : null}
           minDate={minDate ? new Date(minDate.valueOf()) : null}
           maxDate={maxDate ? new Date(maxDate.valueOf()) : null}
           onChange={dateChanged}
@@ -67,5 +73,5 @@ export const DatePicker = ({
         />
       ) : null}
     </InputStyle>
-  );
-};
+  )
+}
