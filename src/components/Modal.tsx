649debe6867de586c37c546f4105c673b8f12247
Jack <jackratner@gmail.com>
2021-03-22 14:58:40 -0400

    feat: reduce header padding in modal
    because the icon button is so robust on its own

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,16 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
-import styled, { ThemeContext } from "styled-components";
-import { rgba } from "polished";
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import ReactDOM from 'react-dom'
+import styled, { ThemeContext } from 'styled-components'
+import { rgba } from 'polished'
 import { DefaultThemeProvider } from './theme'
-import { colors } from "../util/colors";
-import { useBreakpoint } from "../util/hooks";
-import { useScrollPosition } from "../util/scroll";
-import { Container, Item, LayoutWrapper } from "./grid";
-import { Div } from "./spacing";
-import { H2, H3, P } from "./typography";
-import Button from "./Button";
-import IconButton from "./IconButton";
+import { colors } from '../util/colors'
+import { useBreakpoint } from '../util/hooks'
+import { useScrollPosition } from '../util/scroll'
+import { Container, Item, LayoutWrapper } from './grid'
+import { Div } from './spacing'
+import { H2, H3, P } from './typography'
+import Button from './Button'
+import IconButton from './IconButton'
 
 const Overlay = styled(Container)`
   position: fixed;
@@ -20,18 +20,18 @@ const Overlay = styled(Container)`
   right: 0;
   bottom: 0;
   background-color: ${rgba(colors.gray200, 0.5)};
-`;
+`
 
 type WrapperProps = React.ComponentProps<typeof Container> & {
-  size: "big" | "small" | "full";
-};
+  size: 'big' | 'small' | 'full'
+}
 
-const bigWidth = "576px";
-const lilWidth = "320px";
+const bigWidth = '576px'
+const lilWidth = '320px'
 
 const sizeRules = ({ size }: WrapperProps) => {
   switch (size) {
-    case "big":
+    case 'big':
       return `
   @media (max-width: ${bigWidth}) {
     height: 100%;
@@ -43,8 +43,8 @@ const sizeRules = ({ size }: WrapperProps) => {
     width: ${bigWidth};
     border-radius: 8px;
   }
-`;
-    case "small":
+`
+    case 'small':
       return `
   max-height: calc(100% - 2rem);
   border-radius: 8px;
@@ -57,14 +57,14 @@ const sizeRules = ({ size }: WrapperProps) => {
     width: ${lilWidth};
   }
 
-`;
-    case "full":
+`
+    case 'full':
       return `
   height: 100%;
   width: 100vw;
-`;
+`
   }
-};
+}
 
 const ModalWrapper = styled(Container) <WrapperProps>`
   background-color: ${colors.white};
@@ -74,40 +74,40 @@ const ModalWrapper = styled(Container) <WrapperProps>`
   overflow: hidden;
 
   ${sizeRules}
-`;
+`
 
 const AnimateH3 = styled(H3)`
   transition: opacity ease 250ms;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-`;
+`
 
 const Contents = styled(Div)`
   overflow: auto;
   flex-grow: 1;
-`;
+`
 
 const Footer = styled(Div)`
   box-shadow: 0px 0px 8px rgba(41, 40, 39, 0.1);
   background-color: ${colors.white};
-`;
+`
 
 type ModalProps = {
-  open: boolean;
-  back?: boolean;
-  size?: "small" | "big" | "full";
-  banner?: React.ReactNode;
-  titleText?: string;
-  submitColor?: string;
-  submitText?: string;
-  cancelText?: string;
-  onSubmit?: () => any;
-  onCancel?: () => any;
-  onDismiss?: () => any;
-  submitDisabled?: boolean;
-  renderFooter?: () => React.ReactNode;
-};
+  open: boolean
+  back?: boolean
+  size?: 'small' | 'big' | 'full'
+  banner?: React.ReactNode
+  titleText?: string
+  submitColor?: string
+  submitText?: string
+  cancelText?: string
+  onSubmit?: () => any
+  onCancel?: () => any
+  onDismiss?: () => any
+  submitDisabled?: boolean
+  renderFooter?: () => React.ReactNode
+}
 
 const Modal: React.FC<ModalProps> = ({
   open,
@@ -123,66 +123,66 @@ const Modal: React.FC<ModalProps> = ({
   renderFooter,
   banner,
   submitDisabled = false,
-  size = "big",
+  size = 'big',
 }) => {
-  const [titleVisible, setTitleVisible] = useState(false);
-  const rootElemRef = useRef(document.createElement("div"));
+  const [titleVisible, setTitleVisible] = useState(false)
+  const rootElemRef = useRef(document.createElement('div'))
   const theme = useContext(ThemeContext)
 
-  const isMobileViewport = useBreakpoint("sm");
+  const isMobileViewport = useBreakpoint('sm')
 
   useEffect(function setupElement() {
-    const parentElem = document.querySelector("body");
+    const parentElem = document.querySelector('body')
 
-    parentElem.appendChild(rootElemRef.current);
-    return () => rootElemRef.current.remove();
-  }, []);
+    parentElem.appendChild(rootElemRef.current)
+    return () => rootElemRef.current.remove()
+  }, [])
 
   useEffect(() => {
-    if (!open) return;
+    if (!open) return
 
-    const body = document.querySelector("body");
-    const oldOverflow = body.style.overflow;
-    body.style.overflow = "hidden";
+    const body = document.querySelector('body')
+    const oldOverflow = body.style.overflow
+    body.style.overflow = 'hidden'
 
-    return () => (document.querySelector("body").style.overflow = oldOverflow);
-  }, [open]);
+    return () => (document.querySelector('body').style.overflow = oldOverflow)
+  }, [open])
 
   const { scrollRegionRef, watchElementRef: titleRef } = useScrollPosition(
     (pos) => {
       if (!pos.visible && !titleVisible) {
-        setTitleVisible(true);
+        setTitleVisible(true)
       } else if (pos.visible && titleVisible) {
-        setTitleVisible(false);
+        setTitleVisible(false)
       }
     },
     [titleVisible, setTitleVisible]
-  );
+  )
 
-  if (!open) return null;
+  if (!open) return null
 
   const dismiss = () => {
-    onDismiss && onDismiss();
-    onCancel && onCancel();
-  };
+    onDismiss && onDismiss()
+    onCancel && onCancel()
+  }
 
-  let footer = null;
-  let inlineFooterContents = null;
+  let footer = null
+  let inlineFooterContents = null
   if (renderFooter) {
-    const inner = renderFooter();
-    footer = inner ? <Footer p={2}>{inner}</Footer> : null;
+    const inner = renderFooter()
+    footer = inner ? <Footer p={2}>{inner}</Footer> : null
   } else if (onCancel || onSubmit) {
-    const big = size !== "small";
+    const big = size !== 'small'
     const cancelButton = onCancel ? (
       <Button
         secondary
         onClick={dismiss}
         name="Cancel"
-        style={big ? {} : { minWidth: "110px" }}
+        style={big ? {} : { minWidth: '110px' }}
       >
         {cancelText}
       </Button>
-    ) : null;
+    ) : null
     const submitButton = onSubmit ? (
       <Div pl={cancelButton && big ? 2 : 0}>
         <Button
@@ -192,12 +192,12 @@ const Modal: React.FC<ModalProps> = ({
           {...{
             ...(submitColor && { color: submitColor }),
           }}
-          style={big ? {} : { minWidth: "110px" }}
+          style={big ? {} : { minWidth: '110px' }}
         >
           {submitText}
         </Button>
       </Div>
-    ) : null;
+    ) : null
     if (big) {
       footer = (
         <Footer p={2}>
@@ -206,46 +206,46 @@ const Modal: React.FC<ModalProps> = ({
             {submitButton}
           </Container>
         </Footer>
-      );
+      )
     } else {
       inlineFooterContents = (
         <Container py={3} justify="space-around">
           {cancelButton}
           {submitButton}
         </Container>
-      );
+      )
     }
   }
 
-  const headerBorder = size === "full" && !isMobileViewport;
-  const TitleComponent = size === "full" ? H2 : H3;
+  const headerBorder = size === 'full' && !isMobileViewport
+  const TitleComponent = size === 'full' ? H2 : H3
 
   const inner = (
     <>
       {titleText ? (
         <TitleComponent
-          weight={size === "full" ? 700 : 600}
-          pt={size === "small" || headerBorder ? 3 : 0}
+          weight={size === 'full' ? 700 : 600}
+          pt={size === 'small' || headerBorder ? 3 : 0}
           pb={3}
           ref={titleRef}
         >
           {titleText}
         </TitleComponent>
       ) : (
-          (size === "small" || headerBorder) && <Div pb={3} />
+          (size === 'small' || headerBorder) && <Div pb={3} />
         )}
       {children}
       {inlineFooterContents}
     </>
-  );
+  )
 
   return ReactDOM.createPortal(
     <DefaultThemeProvider theme={theme}>
       <Overlay
         center
         onClick={(e: any) => {
-          e.stopPropagation();
-          dismiss();
+          e.stopPropagation()
+          dismiss()
         }}
       >
         <ModalWrapper
@@ -253,26 +253,26 @@ const Modal: React.FC<ModalProps> = ({
           direction="column"
           onClick={(e: MouseEvent) => e.stopPropagation()}
         >
-          {size !== "small" && (
+          {size !== 'small' && (
             <Container
               px={3}
-              py={3}
+              py={2}
               align="center"
-              style={{ flexWrap: "nowrap" }}
+              style={{ flexWrap: 'nowrap' }}
               bb={headerBorder}
             >
               <IconButton
-                icon={back ? "back" : "close"}
+                icon={back ? 'back' : 'close'}
                 name="Close"
                 onClick={dismiss}
               />
-              {!back && size === "full" && !isMobileViewport && (
-                <Div onClick={dismiss} style={{ cursor: "pointer" }} pl={1}>
+              {!back && size === 'full' && !isMobileViewport && (
+                <Div onClick={dismiss} style={{ cursor: 'pointer' }} pl={1}>
                   <P>Close</P>
                 </Div>
               )}
               {back && (
-                <Div onClick={dismiss} style={{ cursor: "pointer" }} pl={1}>
+                <Div onClick={dismiss} style={{ cursor: 'pointer' }} pl={1}>
                   <P>Back</P>
                 </Div>
               )}
@@ -290,7 +290,7 @@ const Modal: React.FC<ModalProps> = ({
           )}
           <Contents ref={scrollRegionRef}>
             {banner ? banner : null}
-            {size === "full" ? (
+            {size === 'full' ? (
               <LayoutWrapper>{inner}</LayoutWrapper>
             ) : (
                 <Div px={3}>{inner}</Div>
@@ -301,7 +301,7 @@ const Modal: React.FC<ModalProps> = ({
       </Overlay>
     </DefaultThemeProvider>,
     rootElemRef.current
-  );
-};
+  )
+}
 
-export default Modal;
+export default Modal
