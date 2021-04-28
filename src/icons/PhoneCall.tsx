5cf7e6a036fbd01c6089cc7a03e40599eb13ab8f
Jack <jackratner@gmail.com>
2021-04-28 17:07:34 -0400

    feat: phatter phone call icon

--- src/icons/PhoneCall.tsx
+++ src/icons/PhoneCall.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react'
 
 const PhoneCall = () => (
   <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M15.05 5A5 5 0 0119 8.95L15.05 5zm0-4A9 9 0 0123 8.94L15.05 1zM22 16.92v3a1.998 1.998 0 01-2.18 2 19.791 19.791 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72c.127.96.361 1.903.7 2.81a2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0122 16.92z"
       stroke="#000"
+      strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
   </svg>
-);
+)
 
-export default PhoneCall;
+export default PhoneCall
