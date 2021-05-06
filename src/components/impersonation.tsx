01c54be2e7604b81815b7cb66f35f632a56ac533
Jack <jackratner@gmail.com>
2021-05-06 14:58:40 -0400

    feat: updated impersonation UI

--- src/components/impersonation.tsx
+++ src/components/impersonation.tsx
@@ -1,72 +1,75 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { Button, colors, Container, Div, H2, IconButton } from "../ui";
-import { isImpersonating, stopImpersonating, impersonate } from "../util/api";
-import type { impersonateStorage } from "../util/api";
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { colors, Container, Div, Item, Link, P, useBreakpoint } from '../ui'
+import { isImpersonating, stopImpersonating, impersonate } from '../util/api'
 
 export const ImpersonationTrigger: React.FC = () => {
-  const query = new URLSearchParams(window.location.search);
-  const impersonateId = query.get("impersonate_id");
+  const query = new URLSearchParams(window.location.search)
+  const impersonateId = query.get('impersonate_id')
 
   useEffect(() => {
-    if (!impersonateId) return;
-    impersonate(impersonateId, { storage: "session" });
-    window.location.replace(window.location.origin);
-  }, [impersonateId]);
+    if (!impersonateId) return
+    impersonate(impersonateId, { storage: 'session' })
+    window.location.replace(window.location.origin)
+  }, [impersonateId])
 
-  return null;
-};
+  return null
+}
 
 const Footer = styled.footer`
   position: fixed;
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: ${colors.red700};
-  z-index: 1000;
-`;
-
-const Dismiss = styled.div`
-  position: absolute;
-  right: 2rem;
-  top: 2rem;
-`;
+  background-color: ${colors.red600};
+  z-index: 1000000;
+`
 
 export const ImpersonationModule = ({
   userName,
   apiBase,
 }: {
-  userName: string;
-  apiBase: string;
+  userName: string
+  apiBase: string
 }) => {
-  const [hidden, setHidden] = useState(false);
+  const [hidden, setHidden] = useState(false)
+  const isNarrow = useBreakpoint('sm')
 
-  if (hidden || !isImpersonating()) return null;
+  if (hidden || !isImpersonating()) return null
 
   const stop = () => {
-    stopImpersonating(apiBase);
-    window.location.replace(window.location.origin);
-  };
+    stopImpersonating(apiBase)
+    window.location.replace(window.location.origin)
+  }
 
   return (
     <Footer>
-      <Container align="center" justify="space-between" p={2}>
-        <H2 weight={700} color={colors.white}>
-          WARNING you are impersonating {userName}
-        </H2>
-        <Container>
+      <Container align="center" justify="space-between" px={3} py={2}>
+        <Item
+          cols={isNarrow ? 12 : 6}
+          pb={isNarrow ? 2 : 0}
+          align="center"
+          justify={isNarrow ? 'center' : 'flex-start'}
+        >
+          <P weight={700} color={colors.white}>
+            You are impersonating {userName}
+          </P>
+        </Item>
+        <Item
+          cols={isNarrow ? 12 : 6}
+          align="center"
+          justify={isNarrow ? 'center' : 'flex-end'}
+        >
           <Div pr={3}>
-            <Button onClick={stop} secondary>
+            <Link underline color={colors.white} onClick={stop}>
               Stop impersonating
-            </Button>
+            </Link>
           </Div>
-          <IconButton
-            onClick={() => setHidden(true)}
-            name="Close footer"
-            icon="close"
-          />
-        </Container>
+          <Link underline color={colors.white} onClick={() => setHidden(true)}>
+            Hide me
+          </Link>
+        </Item>
       </Container>
     </Footer>
-  );
-};
+  )
+}
