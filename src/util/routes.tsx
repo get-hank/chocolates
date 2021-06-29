dcacf339e1b2936ec1c12f62ddaaa06dcc6aeb68
Jack <jackratner@gmail.com>
2021-06-29 15:59:56 -0400

    feat: add smsHref func
    to get a sms url

--- src/util/routes.tsx
+++ src/util/routes.tsx
@@ -1,53 +1,56 @@
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory, useLocation } from 'react-router-dom'
 
 export const useQuery = () => {
-  return new URLSearchParams(useLocation().search);
-};
+  return new URLSearchParams(useLocation().search)
+}
 
-type queryArg = { [key: string]: string } | URLSearchParams;
+type queryArg = { [key: string]: string } | URLSearchParams
 
 export const toRelativeUrl = (path: string, query: queryArg) => {
   if (typeof query !== typeof URLSearchParams)
-    query = new URLSearchParams(query);
-  return `${path}?${query.toString()}`;
-};
+    query = new URLSearchParams(query)
+  return `${path}?${query.toString()}`
+}
 
-type MailTo = (email: string, opts?: { subject?: string }) => string;
+type MailTo = (email: string, opts?: { subject?: string }) => string
 export const mailTo: MailTo = (email, { subject } = {}) => {
-  let link = `mailto:${email}`;
+  let link = `mailto:${email}`
   if (subject) {
-    link = `${link}?subject=${encodeURIComponent(subject)}`;
+    link = `${link}?subject=${encodeURIComponent(subject)}`
   }
-  return link;
-};
+  return link
+}
 
-type MaybeString = string | null | undefined;
-type ParamSetter = (p?: MaybeString) => any;
-type URLGetter = (p?: MaybeString) => string;
+export const smsHref = (body: string) =>
+  `sms:?&=body=${encodeURIComponent(body)}`
+
+type MaybeString = string | null | undefined
+type ParamSetter = (p?: MaybeString) => any
+type URLGetter = (p?: MaybeString) => string
 export const useQueryParam = (
   paramName: string,
   render?: (renderProps: {
-    paramValue: MaybeString;
-    setParam: ParamSetter;
+    paramValue: MaybeString
+    setParam: ParamSetter
   }) => React.ReactNode
 ) => {
-  const { pathname } = useLocation();
-  const history = useHistory();
-  const query = useQuery();
-  const paramValue = query.get(paramName);
+  const { pathname } = useLocation()
+  const history = useHistory()
+  const query = useQuery()
+  const paramValue = query.get(paramName)
   const getUrl: URLGetter = (newVal) => {
-    const newQuery = new URLSearchParams(query);
-    newVal ? newQuery.set(paramName, newVal) : newQuery.delete(paramName);
-    return toRelativeUrl(pathname, newQuery);
-  };
+    const newQuery = new URLSearchParams(query)
+    newVal ? newQuery.set(paramName, newVal) : newQuery.delete(paramName)
+    return toRelativeUrl(pathname, newQuery)
+  }
   const setParam: ParamSetter = (newVal) => {
-    history.push(getUrl(newVal));
-  };
+    history.push(getUrl(newVal))
+  }
 
   return {
     setParam,
     getUrl,
     paramValue,
     ...(render && { rendered: render({ paramValue, setParam }) }),
-  };
-};
+  }
+}
