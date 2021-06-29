126febaacf58b82de8ee9bfadbc42ebe6edc5c52
Jack <jackratner@gmail.com>
2021-06-29 09:51:07 -0400

    feat: copy button link variant

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from './Link'
 import { Div } from './spacing'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 type CopyButtonProps = React.ComponentProps<typeof Div> & {
   text: string
   buttonProps?: React.ComponentProps<typeof Button>
+  linkProps?: React.ComponentProps<typeof Link>
   label?: string
+  variant?: 'button' | 'link'
 }
 
 const CopyButton = ({
   text,
   label,
   buttonProps,
+  linkProps,
+  variant = 'link',
   ...divProps
 }: CopyButtonProps) => {
   return (
     <CopyToClipboard text={text}>
       <Div {...divProps}>
-        <Button>{label || 'Copy'}</Button>
+        {variant === 'button' && (
+          <Button {...buttonProps}>{label || 'Copy'}</Button>
+        )}
+        {variant === 'link' && <Link {...linkProps}>{label || 'Copy'}</Link>}
       </Div>
     </CopyToClipboard>
   )
