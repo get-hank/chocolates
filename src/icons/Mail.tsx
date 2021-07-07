a980dba6817bfba631d6463c21a471e07c300ecf
Jack <jackratner@gmail.com>
2021-07-07 09:47:31 -0400

    feat: Mail and MessageCircle icons

---
+++ src/icons/Mail.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Icon = () => (
+  <svg width="24" height="25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4 4.5h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2v-12c0-1.1.9-2 2-2z"
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="m22 6.5-10 7-10-7"
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default Icon
---
+++ src/icons/MessageCircle.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Icon = () => (
+  <svg width="24" height="25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M21 12a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.379 8.379 0 0 1-3.8-.9L3 21.5l1.9-5.7A8.38 8.38 0 0 1 4 12a8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default Icon
